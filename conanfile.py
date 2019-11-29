c07ed536f7e6658b7b522952358f1cd6701f21b0
stefan <stefan.eicher@gmail.com>
2019-11-29 15:33:17 +0100

    update

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,14 @@
 from conans import ConanFile, CMake
 
 class Calculator(ConanFile):
+    name = "BluetoohLib"
+    version = "0.1"
+    url = "Basic-Calculator"
+    description = "<Description of Hello here>"
     settings = "os", "compiler", "build_type", "arch"
     requires = "doctest/2.3.4@bincrafters/stable" # comma-separated list of requirements
     generators = "cmake", "gcc", "txt"
+    exports_sources = "src/*", "test/*", "CMakeLists.txt"
 
     def configure_cmake(self):
         cmake = CMake(self)
@@ -12,6 +17,7 @@ class Calculator(ConanFile):
         # build() and package()
         cmake.definitions["BUILD_UTEST"] = "ON"
         cmake.definitions["USE_COVERAGE"] = "OFF"
+        cmake.definitions["CMAKE_BUILD_TYPE"] = "Debug"
 
         cmake.configure()
         return cmake
@@ -20,7 +26,12 @@ class Calculator(ConanFile):
         cmake = CMake(self)
         cmake = self.configure_cmake()
         cmake.build()
-        # run unit tests after the build
         cmake.test()
-        # self.run("../show_coverage.sh")
-        
+        #self.run("../show_coverage.sh")
+
+    def package(self):
+        self.copy("*.hpp", dst="include", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["BluetoohLib"]
