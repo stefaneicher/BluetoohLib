e8c00d59a2415cf0ac2afe66c8b6ecbc1ae927ee
stefan <stefan.eicher@gmail.com>
2019-12-02 13:48:38 +0100

    update

--- createWithDocker.sh
+++ createWithDocker.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-docker run -i --name BluetoohLib -v/Users/stefaneicher/.conan:/home/conan/.conan -v "$(pwd)":/buildFolder:ro conanio/gcc63 bash -c -c "cd /buildFolder && ls -l && ./create.sh" --rm
+docker run -i \
+-v /Users/stefaneicher/.conan:/home/conan/.conan \
+-v "$(pwd)":/buildFolder:ro conanio/gcc63 \
+bash \
+-c "cd /buildFolder && ls -l && ./create.sh" \
+--rm
+#--name BluetoohLib \
+
