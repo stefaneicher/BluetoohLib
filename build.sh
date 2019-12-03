0151bcc4cae1d46aaae71a17218beff8159df7bc
stefan <stefan.eicher@gmail.com>
2019-12-03 13:56:47 +0100

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-conan install . --install-folder cmake-build-debug
+conan install . --install-folder cmake-build-debug -s build_type=Debug
 conan build . --build-folder cmake-build-debug
--- create.sh
+++ create.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
-#conan remove "*" -f
-#conan search "*"
-conan create . user/testing -pr=./profiles/apple_clang
+conan create . user/testing -s build_type=Debug
+#conan create . user/testing -pr=./profiles/apple_clang
 #conan create . user/testing -pr=./profiles/linux_gcc
-#conan search "*"
